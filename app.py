f3f1074209ebee6284b1abcd020bea0f65abd0d4
Humberto Machado <hmaxsuel25@gmail.com>
2021-10-16 11:56:25 -0300

    otimização
    otimização de código

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-import os
-import sys
-import platform
-import locale
 import smtplib
-import mimetypes
 from optparse import OptionParser
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-from wordpass import senha
-from var import menu
-from var import words
+from wordpass import senha, user, ports, hosts
+from var import menu, words
 
 #config smtp
-host = ""
-port = ""
-login = ""
+host = hosts
+port = ports
+login = user
 password = senha
 
 server = smtplib.SMTP(host, port)
--- wordpass.py
+++ wordpass.py
@@ -1,1 +1,4 @@
+hosts = 'colocar host'
+user = 'colcar endereço de email'
 senha = 'colocar senha'
+ports = 'colocar porta de saida'
