f5140bcc269cd6fe03966cd4566f072b3cbd3541
Humberto Machado <hmaxsuel25@gmail.com>
2021-10-22 12:09:58 -0300

    Adição novas variaveis

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-import smtplib
+import smtplib, ssl
 from optparse import OptionParser
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-from wordpass import senha, user, ports, hosts
-from var import menu, words
+#variaveis configuráveis
+from wordpass import senha, user, ports, hosts, destinatario
+#variaveis de menu
+from var import menu, menu2
 
 #config smtp
 host = hosts
@@ -19,12 +21,10 @@ server.ehlo()
 server.starttls()
 server.login(login, password)
 
-
-#Construção
-corpo = words
+#Construção do email
 email_msg = MIMEMultipart()
 email_msg['From'] = login
-email_msg['To'] = ''
-email_msg['Subject'] = menu
+email_msg['To'] = destinatario
+email_msg['Subject'] = function(menu+menu2)
 
 server.quit()
--- var.py
+++ var.py
@@ -7,6 +7,7 @@ def menu():
             [F] - Fábrica
             [T] - Loja Tamoios
             [U] - Loja Tupis
+            [I] - Loja Ipatinga
             [S] - Sair
         ''')
     str(input('Escolha uma opção: '))
@@ -16,8 +17,30 @@ br040esc = "e"
 brfabrica = "f"
 brtamoios = "t"
 brtupis = "u"
+bripatinga = "i"
 sair = "s"
 
 menu()
+if (br040vendas, br040esc, brfabrica, brtamoios, brtupis, bripatinga): 
+    print ('descreva o problema:')
+    def menu2():
+        print('''
+            PROBLEMA:
+
+            [P] Periféricos (Teclado, Mouse, Monitor, etc.)
+            [T] Suprimentos (Toner, Cilindo de impressão)
+            [C] Suporte Técnico (Computador)
+            [S] Suporte Técnico (Sistemas Integrados)
+            [A] Acessos (Permissão de acessos) 
+            [V] Voltar ao menu anterior
+        ''')
+        str(input('Escolha uma Opção:'))
+perif = "p"
+suprir = "t"
+compt = "c"
+suport = "s"
+acess = "a"
+voltar = "v"
+
 if (sair):
     print ('Programa Finalizado')
